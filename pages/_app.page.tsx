6e437a5baf34268832a45f6ba2a0e71b7ab53fe2
李思源 <2574561698@qq.com>
2024-02-15 23:31:49 +0800

    app.page.tsx  自定义返回数据

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
-}
+};
+
+App.getInitialProps = async () => {
+  // 使用自定义的函数获取共享数据，并将其作为 pageProps 返回
+  // 将共享数据作为 pageProps 返回，使其能够在所有页面中使用
+  return { pageProps: { sharedData: 1 } };
+};
+export default App;
