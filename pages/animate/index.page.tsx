c1e14bd0db988826dd60023e4d32b97ce3998b2b
lisy999 <2574561698@qq.com>
2024-05-23 10:36:23 +0800

    feat(新增模块)animate style

---
+++ pages/animate/index.page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "animate.css";
+export default function Animate() {
+  return (
+    <div className="m-10">
+      <h1 className="animate__animated animate__heartBeat">
+        An animated element
+      </h1>
+    </div>
+  );
+}
